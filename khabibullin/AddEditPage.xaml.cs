abe8dcd03c5268a8425b4d765bdf013acc7a7acb
timurhab189 <timurhab189@mail.ru>
2024-10-16 16:26:39 +0500

    6 лаба

--- khabibullin/AddEditPage.xaml.cs
+++ khabibullin/AddEditPage.xaml.cs
@@ -15,14 +15,52 @@ using System.Windows.Shapes;
 
 namespace khabibullin
 {
-    /// <summary>
-    /// Логика взаимодействия для AddEditPage.xaml
-    /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Service _currentService = new Service();
+
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+
+            if (SelectedService != null)
+                _currentService = SelectedService;
+
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("Укажите название услуги");
+            if(_currentService.Cost <= 0)
+                errors.AppendLine("Укажите стоимость услуги");
+            if(_currentService.Discount == null)
+                errors.AppendLine("Укажите скидку");
+            if(string.IsNullOrWhiteSpace(_currentService.Duration))
+                errors.AppendLine("Укажите длительность услуги");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentService.ID == 0)
+            {
+                Khabibullin_autoserviceEntities.GetContext().Service.Add(_currentService);
+            }
+            try
+            {
+                Khabibullin_autoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
--- khabibullin/ServicePage.xaml.cs
+++ khabibullin/ServicePage.xaml.cs
@@ -68,10 +68,6 @@ namespace khabibullin
                 ServiceListView.ItemsSource = currentServices.OrderBy(p => p.Cost).ToList();
             }
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new AddEditPage());
-        }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -91,6 +87,25 @@ namespace khabibullin
         private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
         {
             UpdateServices();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Khabibullin_autoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = Khabibullin_autoserviceEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
